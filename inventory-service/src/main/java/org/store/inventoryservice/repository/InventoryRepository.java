9db4158dfdf981e30289fdf042d2b380eebe0ec7
Romych <lutsiv55@gmail.com>
2023-10-23 16:51:16 +0300

    Add findByScuCodeIn method to InventoryRepository. Add Javadocs.

--- inventory-service/src/main/java/org/store/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/org/store/inventoryservice/repository/InventoryRepository.java
@@ -3,8 +3,20 @@ package org.store.inventoryservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.store.inventoryservice.model.Inventory;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory ,Long> {
+
+    /**
+     * @param skuCode you want to find
+     * @return Optional with Inventory model you trying to find.
+     */
     Optional<Inventory> findBySkuCode(String skuCode);
+
+    /**
+     * @param skuCodes list of scuCodes.
+     * @return list of Inventory models with present scuCodes.
+     */
+    List<Inventory> findBySkuCodeIn(List<String> skuCodes);
 }
